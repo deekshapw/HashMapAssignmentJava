8e8f027afbcb323d967589bdd5b861f5bdb33ad3
Deeksha Gangwar <123671394+deekshapw@users.noreply.github.com>
2024-03-31 01:11:46 +0530

    Create Assgn2.java

---
+++ Assgn2.java
@@ -0,0 +1,17 @@
+import java.util.TreeMap;
+
+public class Assgn2
+{
+    public static void main(String[] args)
+    {
+        TreeMap<String , Integer> map2 = new TreeMap<>();
+        map2.put("Krish", 4);
+        map2.put("Lakshay", 2);
+        map2.put("Lakshman", 7);
+        map2.put("Rahul", 5);
+        map2.put("Ram", 1);
+        System.out.println(map2);
+        
+    }
+    
+}
